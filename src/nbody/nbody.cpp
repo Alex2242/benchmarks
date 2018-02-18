aba4b16ebe9d1af1bf189402e40b11fdfa5810f3
Alex2242 <alex@degurse.fr>
2018-02-18 12:58:54 +0100

    Error in indent corrected

--- src/nbody/nbody.cpp
+++ src/nbody/nbody.cpp
@@ -23,7 +23,8 @@ Nbody::Nbody(int n, float dt, int mass ) {
 		z[i] = dist(mt);
 		vx[i] = 0.0 ;
 		vy[i] = 0.0 ;
-	 	vz[i] = 0.0 ;}
+		vz[i] = 0.0 ;
+	}
 
 	positions = new double*[3];
 	positions[0] = x;
@@ -46,19 +47,19 @@ double Nbody::theta(double x1,double x2,double y1,double y2) {
 void Nbody::compute2D() {
 	double fx,fy;
 	for (int tB = 0; tB < nBodies; tB++) { // tB = thisBody
-	    fx = 0; fy = 0;
-	    for (int oB = 0; oB < nBodies; oB++){ // oB = otherBody
+		fx = 0; fy = 0;
+		for (int oB = 0; oB < nBodies; oB++){ // oB = otherBody
 			if (oB != tB){
-		     	fx += G * pow(m,2) * (x[oB] - x[tB]) / (pow(abs(x[oB] - x[tB]), 2) + 0.01 );
-		    	fy += G * pow(m,2) * (y[oB] - y[tB]) / (pow(abs(y[oB] - y[tB]), 2) + 0.01 );
+				fx += G * pow(m,2) * (x[oB] - x[tB]) / (pow(abs(x[oB] - x[tB]), 2) + 0.01 );
+				fy += G * pow(m,2) * (y[oB] - y[tB]) / (pow(abs(y[oB] - y[tB]), 2) + 0.01 );
 			}
 		}
 
-	    vx[tB] += fx * dt / m;
-	    x[tB]  += vx[tB] * dt;
+		vx[tB] += fx * dt / m;
+		x[tB]  += vx[tB] * dt;
 
-	    vy[tB] += fy * dt / m;
-	    y[tB]  += vy[tB] * dt;
+		vy[tB] += fy * dt / m;
+		y[tB]  += vy[tB] * dt;
 	}
 }
 
@@ -66,19 +67,20 @@ void Nbody::compute2D() {
 void Nbody::compute3D() {
 	double fx,fy,fz;
 	for (int tB = 0; tB < nBodies; tB++) { // tB = thisBody
-	    fx = 0; fy = 0; fz = 0;
-	    for (int oB = 0; oB < nBodies; oB++){ // oB = otherBody
+		fx = 0; fy = 0; fz = 0;
+		for (int oB = 0; oB < nBodies; oB++){ // oB = otherBody
 			if (oB != tB){
-		     	fx += G * pow(m,2) * (x[oB] - x[tB]) / (pow(abs(x[oB] - x[tB]), 3) + 0.01 );
-		    	fy += G * pow(m,2) * (y[oB] - y[tB]) / (pow(abs(y[oB] - y[tB]), 3) + 0.01 );
+				fx += G * pow(m,2) * (x[oB] - x[tB]) / (pow(abs(x[oB] - x[tB]), 3) + 0.01 );
+				fy += G * pow(m,2) * (y[oB] - y[tB]) / (pow(abs(y[oB] - y[tB]), 3) + 0.01 );
 				fz += G * pow(m,2) * (z[oB] - z[tB]) / (pow(abs(z[oB] - z[tB]), 3) + 0.01 );
 			}
 		}
-	    vx[tB] += fx * dt / m;
-	    x[tB]  += vx[tB] * dt;
 
-	    vy[tB] += fy * dt / m;
-	    y[tB]  += vy[tB] * dt;
+		vx[tB] += fx * dt / m;
+		x[tB]  += vx[tB] * dt;
+
+		vy[tB] += fy * dt / m;
+		y[tB]  += vy[tB] * dt;
 
 		vz[tB] += fz * dt / m;
 		z[tB]  += vz[tB] * dt;
